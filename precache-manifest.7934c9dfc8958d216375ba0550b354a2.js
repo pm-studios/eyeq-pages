66e986a0edef05b0ecb484cb0031968f02c0239c
limdp99 <dlim@pm-studios.com>
2022-08-11 11:14:05 +0900

    Updates

---
+++ precache-manifest.7934c9dfc8958d216375ba0550b354a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e4f59e3b36ad1b5973e",
+    "url": "/eyeq-pages/static/css/main.8b458d6d.chunk.css"
+  },
+  {
+    "revision": "9e4f59e3b36ad1b5973e",
+    "url": "/eyeq-pages/static/js/main.9e4f59e3.chunk.js"
+  },
+  {
+    "revision": "9d574cb979eb7e0f42c4",
+    "url": "/eyeq-pages/static/css/1.fa98b4c6.chunk.css"
+  },
+  {
+    "revision": "9d574cb979eb7e0f42c4",
+    "url": "/eyeq-pages/static/js/1.9d574cb9.chunk.js"
+  },
+  {
+    "revision": "1af4ad114358be036e79",
+    "url": "/eyeq-pages/static/js/runtime~main.1af4ad11.js"
+  },
+  {
+    "revision": "90fe2529818ab8aa31f1d0fc6af4d3aa",
+    "url": "/eyeq-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://https://pm-studios.github.io/eyeq-pages/precache-manifest.3a1c904270deb7c9f175ea6dced0a6ac.js"
+  "/eyeq-pages/precache-manifest.7934c9dfc8958d216375ba0550b354a2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://https://pm-studios.github.io/eyeq-pages/index.html", {
+workbox.routing.registerNavigationRoute("/eyeq-pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
